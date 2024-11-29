df54241036955f2a9126c00d7c72f75533c48da6
Marat <Marsiktop07@yandex.ru>
2024-11-29 13:03:21 +0300

    Added 4.5.py

---
+++ 4_sem/4.5.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from numpy import array
+
+
+df = pd.read_csv('BTC_data.csv')
+cl = array(df['close'])
+t = array(df['time'])
+fig = plt.figure(figsize=(16, 9))
+ax = fig.add_subplot(111)
+ax.scatter(t, cl)
+ax.set_title('BTC Price')
+ax.set_xlabel('Time')
+ax.set_ylabel('Price')
+ax.plot(t, cl)
+plt.show()
